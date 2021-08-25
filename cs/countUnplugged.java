8935969280c543e737ffd56ecaa670ae4b200ebe
TheArctesian <87273292+TheArctesian@users.noreply.github.com>
2021-08-25 13:21:54 +0800

    fix this shit later pls

---
+++ cs/countUnplugged.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.io.File;
+import java.util.Scanner;
+
+public class countUnplugged 
+{
+	public static void main (String[] args) throws Exception 
+	{
+		File data = new File("C:/Users/steph/Desktop/Hackerman/SchoolFiles/cs/counting");
+		
+		System.out.println("working");
+		while (fileRead != null){
+			
+		}
+		while (sc.hasNextLine())
+	{	
+				System.out.println("working");
+
+				{
+			System.out.println(sc.nextLine());
+		}
+		sc.close();
+	}
+}
+
