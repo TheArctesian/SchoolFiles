2175fb791fb70ba2e6eb045a73e0dc901049a7c9
TheArctesian <87273292+TheArctesian@users.noreply.github.com>
2021-08-31 07:59:02 +0800

    act finished ish

--- cs/countUnplugged.java
+++ cs/countUnplugged.java
@@ -1,25 +1,59 @@
 import java.io.*;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 
 public class countUnplugged 
 {
 	public static void main (String[] args) throws Exception 
 	{
-		File data = new File("C:/Users/steph/Desktop/Hackerman/SchoolFiles/cs/counting");
-		
-		System.out.println("working");
-		while (fileRead != null){
+		HashMap<String, Integer> something = new HashMap<String,Integer>();
+		BufferedReader Reader = null; 
+		try {
+			Reader = new BufferedReader(new FileReader("C:\\Users\\steph\\Desktop\\Hackerman\\SchoolFiles\\cs\\test.txt"));
+			String currentLine = Reader.readLine();
+			while(currentLine!= null) {
+//				String words [] = currentLine.toLowerCase().split(" ");
+				String words [] = currentLine.replaceAll("[^0-9a-zA-Z\s]", "").toLowerCase().split(" ");
+				
+				for (String word:words) {
+					if(something.containsKey(word)) {
+						something.put(word, something.get(word)+1);
+					}
+					else {
+						something.put(word, 1);
+					}
+				}
+//				System.out.println(words.toString());
+				currentLine=Reader.readLine();
+			}
 			
+			String repeated=null; 
+			int count = 0; 
+			Set<Entry<String,Integer>> entrySet=something.entrySet();
+			for(Entry<String, Integer> entry:entrySet){
+				if(entry.getValue()>count) {
+					repeated=entry.getKey();
+					count=entry.getValue();
+				}
+			}
+			System.out.println("most common word is: " +repeated);
+			System.out.println("it came up "+count);
 		}
-		while (sc.hasNextLine())
-	{	
-				System.out.println("working");
-
-				{
-			System.out.println(sc.nextLine());
+		catch(IOException e) {
+			e.printStackTrace();
+			System.out.println("not Working");
 		}
-		sc.close();
+		finally {
+			try {
+				Reader.close();
+			}
+			catch (IOException f) {
+				f.printStackTrace();
+				System.out.println("lmao");
+			}
+		}
 	}
 }
-
