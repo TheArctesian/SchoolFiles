0a80e2d6d51bed2a0f1eb5ba4e6f9d35147b4931
TheArctesian <stephen.d.okita@gmail.com>
2021-09-29 22:12:10 +0800

    added a c++ file but this shit does not work

---
+++ physics/convert.cpp
@@ -0,0 +1,56 @@
+/**
+ * Author = @theArctesian (gitHub) or Stephen Okita
+ * Note, this shit does not work at all 
+ * */
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+float toM(string units, int inputLength){
+    float toMeters;
+    if (units == "km") {
+        toMeters = inputLength*1000;
+    }
+    else if (units == "m"){
+        toMeters = inputLength;
+    }
+    else{
+        cout << "Error in input" << endl;
+    }
+
+    return toMeters; 
+}
+int converted(float convertedToMeters, int expontent){
+    int sciExpo = pow(10, expontent);
+    int number = convertedToMeters * sciExpo;
+    
+    return number;
+}
+
+int main(){
+    float inputLength;
+    cout << "Input a distance: ";
+    cin >> inputLength;
+
+    string units;
+    cout << "is it in meters or km \nm/km: ";
+    cin >> units;
+
+    int expontent;
+    cout << "Add What power of 10 your is to: ";
+    cin >> expontent;
+    
+    float convertVal;
+    int convertedToMeters; 
+    convertedToMeters = toM(units, inputLength);
+
+    // convertVal = converted(convertedToMeters, expontent);
+    // string convertValSci = convertedToMeters + "x 10^" + expontent; 
+
+    cout << "Your length is ";
+    cout << convertVal << endl;
+    cout << "\n or " << convertedToMeters << "x + 10^" << expontent << endl;
+    return 0;
+}
