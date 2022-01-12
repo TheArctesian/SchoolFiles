ae09a86f5cc3bffde6cb844278455926b447beda
TheArctesian <stephen.d.okita@gmail.com>
2022-01-12 21:41:38 +0800

    lmao that took was kinda too easy

--- cs/3.lab/Store.java
+++ cs/3.lab/Store.java
@@ -14,7 +14,7 @@ public class Store
      *  3. You can use assertTrue(true) at the end of the method
      */
     public static void main(String[] args) {
-        Store newStore = new Store();
+        
 
         // Book testBook = new Book("Brave new World", "London", 69, "Dystopian Future", 696969, "20/4/20", "Huxley", "idk what is BN is", "First Edition", "BNW");
         Book testBook = new Book();
@@ -36,6 +36,13 @@ public class Store
      *  4. You can use assertTrue(true) at the end of the method
      */
 
+    Store newStore = new Store();
+    Book newBook = new Book();
+    Phone newPhone = new Phone();
+    newStore.addBook(newBook);
+    newStore.addPhone(newPhone);
+
+
     /*
      * TODO 7: Lagos wants to move all Phones to a new location. Create a method
      *  called updatePhonesLocation() which takes in a location as an argument and
@@ -44,6 +51,7 @@ public class Store
     public void updatePhonesLocation(String location){
         for(CISitem item: lagosItem){
             item.setLocation(location);
+            // assertEquals(item.getLocation(), "location");
         }
     }
     /*
@@ -51,8 +59,8 @@ public class Store
      *  all the phones in the store.
      */
 
-    public arrayList GetAllPhones(){
-        
+    public ArrayList<phone> GetAllPhones(){
+        return lagosItem;
     }
 
     /*
@@ -67,6 +75,17 @@ public class Store
      *  Take a look at tests from other labs to see how assertEquals() is used.
      */
 
+        Store room514 = new store();
+        Phone one = new Phone();
+        Phone two = new Phone();
+        Phone three = new Phone();
+        room514.addPhone(one);
+        room514.addPhone(two);
+        room514.addPhone(three);
+        room514.updatePhonesLocation("Room 514");
+        for (Phone phone: room514.getAllPhones()){
+            assertEquals(phone.getLocation(), "Room 514");
+        }
     /*
      * TODO  10: You can see how creating a method for each specific thing that you
      *  can do with each device will get incredibly tedious. Let's make a method called
@@ -75,25 +94,65 @@ public class Store
      *  will return all books in the store.
      */
 
+        public ArrayList<CISitem> getItems(String type){
+            ArrayList<CISitem> items = new ArrayList<CISitem>();
+            for(CISitem item: lagosItem){
+                if(item.getType().equals(type)){
+                    items.add(item);
+                }
+            }
+            return items;
+        }
     /*
      * TODO 11: Write a test that makes sure that getItems() works correctly. Test it with
      *  with more than one type of CISItem
      */
-
+            
+            Store newStore = new Store();
+            Book newBook = new Book();
+            Phone newPhone = new Phone();
+            newStore.addBook(newBook);
+            newStore.addPhone(newPhone);
+            ArrayList<CISitem> newItems = newStore.getItems("phone");
+            assertEquals(newItems.size(), 1);
+            assertEquals(newItems.get(0).getType(), "phone");
+            ArrayList<CISitem> newItems = newStore.getItems("book");
+            assertEquals(newItems.size(), 1);
+            assertEquals(newItems.get(0).getType(), "book");
     /*
      * TODO 12: Create a method updateItems(String itemType, String property, String value) where itemType
      *  is the type of item the user wants to update. updateItem("phone", "location", "Room513")
      *  will update all phones' location to "Room 513"
      */
-
+        public String updateItems(String type, String property, String value){
+            for(CISitem item: lagosItem){
+                if(item.getType().equals(type)){
+                    item.setProperty(property, value);
+                }
+            }
+            return value;
+        }
     /*
      * TODO 12: Make a test for updateItems that makes sure it works correctly. Try a variety of
      *  combinations.
      */
-
+        Store newStore = new Store();
+        Book newBook = new Book();
+        Phone newPhone = new Phone();
+        newStore.addBook(newBook);
+        newStore.addPhone(newPhone);
+        newStore.updateItems("phone", "location", "Room513");
+        for (CISitem item: newStore.getItems("phone")){
+            assertEquals(item.getLocation(), "Room 513");
+        }
     /*
      * TODO 13: Make method showAllInfo() that prints the information for all items in the store.
      *  You don't need to write a test for this, just verify it manually. Take a look at the console and
      *  make sure things are working correctly.
      */
+
+     public void showAllInfo(){
+        for(CISitem item: lagosItem){
+            System.out.println(item.toString());
+        }
 }
