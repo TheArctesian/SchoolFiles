5ab11289dbdc8a65574f7d7a8267a3b93237e6e4
theArctesian <stephen.d.okita@gmail.com>
2022-12-06 12:50:13 +0800

    hello again

---
+++ Phsyc/cardmaker.py
@@ -0,0 +1,19 @@
+cog = ["Multi-Store Model", "Working Memory Model", "Schema", "Alter et Al()", "Loftus & Pickrell", "Loftus & Palmer", "Reconstructive Memory", "Cognitive bias", "Tversky & Kahneman, Strack and Mussweiller"]
+
+abnorm = ["Genetics", "Kendler", "Vulnerability factors", "Protective factors", "Aaron Beck", "Alloy", "Joiner", "Brown & Harris", "prevalence", "emic", "etic"]
+
+socul = ["Social identity theory", "Social cognitive theory", "Enculturation", "Stereotypes", "Cultural dimensions", "Acculturation", "Bandura", "Tajfel", "Hamilton & Gifford"]
+
+bio = ['Localization', 'Neuroplasticity', 'Neurotransmitter', 'Agonist', 'Antagonist', 'Hormone', 'Pheromone', 'Genetics', 'Adrenaline', 'Acetylcholine', 'Eugene Pauly', 'HM', 'Maguirem', 'Kendler', 'Wedekind', 'Cahill & McGaugh']
+
+EMT = ["Protection from undue harm", "consent", "debrief", "confidentiality", "anonymity", "natural experiment", "quasi experiment", "survey", "meta-analysis", "questionnaire", "semi-structured interview", "unstructured interview", "structured interview"]
+
+ERQ = ['Explain one research method used to study the brain in relation to behaviour.',	
+       'Explain one ethical consideration relevant to one study of the brain in relation to behaviour.',
+       'Explain one technique used to study the brain in relation to behavior.',
+       'Explain localization of function using one study.',
+       'Explain neuroplasticity using one study.',
+       'Explain the effect of one neurotransmitter on one behaviour.',
+        'Explain one research method used to study a hormone in relation to behaviour.',
+        'Explain one ethical consideration relevant to one study of a hormone in relation to behaviour.',
+       ]
